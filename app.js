b933752fedab7c7720b7741c84201564b9cc862d
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2021-06-01 20:11:46 +0530

    basic html layout

---
+++ app.js
@@ -0,0 +1,7 @@
+let btnTranslate = document.querySelector('#btnTranslate');
+let txtInput = document.querySelector("#txtinput")
+btnTranslate.addEventListener("click", clcikHandler());
+
+function clcikHandler() {
+    console.log("oho")
+}
