53a28bdd047b823994813b498bef4cb64a21b47e
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2021-06-20 13:16:49 +0530

    reformat

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 let btnTranslate = document.querySelector('#btnTranslate');
-let txtInput = document.querySelector("#txtinput")
-btnTranslate.addEventListener("click", clcikHandler());
+let txtInput = document.querySelector("#txtinput");
+let outputDiv = document.querySelector("#output")
 
-function clcikHandler() {
-    console.log("oho")
+
+let URL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text){
+    return URL + "?" + "text=" + text
 }
+function errorhandler(error){
+    console.log("error occured",error);
+    alert("server down try later!")
+}
+function clickHandler() {
+    let txtinput = txtInput.value;
+    fetch(getTranslationURL(txtinput)).then(res => res.json()).then(json => {
+        outputDiv.innerText = "translated -" + json.contents.translated}).catch(errorhandler)
+   
+};
+
+btnTranslate.addEventListener("click", clickHandler);
